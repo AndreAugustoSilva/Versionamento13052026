1068c7596cbc6abc25c1fd6f98da4a8f2eada6d7
AndreAugustoSilva <andre_augusto-silva@estudante.sesisenai.org.br>
2026-05-13 21:19:07 -0300

    Initial Commit

---
+++ calculadora.py
@@ -0,0 +1,58 @@
+# =========================
+# CALCULADORA EM PYTHON
+# Operações Básicas
+# =========================
+
+def soma(a,b):
+    return a + b
+
+def multip(a,b):
+    return a * b
+
+def divisao(a,b):
+    if b == 0:
+        return "Div0"
+    else:
+        return a / b
+
+def subtracao(a,b):
+    return a - b
+
+while True:
+    print("\n- CALCULADORA -")
+    print("1 - Soma")
+    print("2 - Multiplicação")
+    print("3 - Divisão")
+    print("4 - Subtração")
+    print("0 - Sair.")
+    
+    opcao = int(input("Escolha a operação: "))
+    
+    if opcao == 0:
+        break
+    
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    
+    if opcao == 1:
+        
+        resultado = soma(num1, num2)
+        print(f"Resultado = {resultado}")
+        
+    elif opcao == 2:
+        
+        resultado = multip(num1, num2)
+        print(f"Resultado = {resultado}")
+        
+    elif opcao == 3:
+        
+        resultado = divisao(num1, num2)
+        print(f"Resultado = {resultado}")
+        
+    elif opcao == 4:
+        
+        resultado = subtracao(num1, num2)
+        print(f"Resultado = {resultado}")
+         
+    else:
+        print("ID Inválido. Tente novamente.")
